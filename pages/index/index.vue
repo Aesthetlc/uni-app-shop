eca0ee16cfdd75683d6002c8c45eb49e98e19adc
yangronghui <35282196+Aesthetlc@users.noreply.github.com>
2021-06-21 00:33:36 +0800

    ’update‘

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<!-- https://www.showdoc.com.cn/128719739414963/2513235043485226 -->
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
