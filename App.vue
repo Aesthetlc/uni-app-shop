b54a7d3c286ef4237b2dbf5a7f7c9fb6f4297f33
yangronghui <35282196+Aesthetlc@users.noreply.github.com>
2021-06-22 00:35:19 +0800

    ’首页轮播图‘

--- App.vue
+++ App.vue
@@ -14,4 +14,33 @@
 
 <style>
 	/*每个页面公共css */
+	@font-face {
+		font-family: "iconfont";
+		/* Project id  */
+		src: url('~@/static/font/iconfont.ttf?t=1624291978489') format('truetype');
+	}
+
+	.iconfont {
+		font-family: "iconfont" !important;
+		font-size: 16px;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.icon-lianxiwomen:before {
+		content: "\e686";
+	}
+
+	.icon-chaoshi:before {
+		content: "\e602";
+	}
+
+	.icon-shipin:before {
+		content: "\e624";
+	}
+
+	.icon-tupian:before {
+		content: "\e607";
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,29 @@
 <template>
-	<view>
+	<view class="home">
 		<!-- https://www.showdoc.com.cn/128719739414963/2513235043485226 -->
-		首页
+		<swiper indicator-dots circular>
+			<swiper-item v-for="item in swiperArr" :key="item.goods_id">
+				<image :src="item.image_src" mode=""></image>
+			</swiper-item>
+		</swiper>
+		<view class="nav">
+			<view class="nav-item">
+				<view class="iconfont icon-chaoshi"></view>
+				<text>超市</text>
+			</view>
+			<view class="nav-item">
+				<view class="iconfont icon-lianxiwomen"></view>
+				<text>联系我们</text>
+			</view>
+			<view class="nav-item">
+				<view class="iconfont icon-tupian"></view>
+				<text>社区图片</text>
+			</view>
+			<view class="nav-item">
+				<view class="iconfont icon-shipin"></view>
+				<text>社区视频</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -9,18 +31,72 @@
 	export default {
 		data() {
 			return {
-
+				swiperArr: [] //轮播图数据
 			}
 		},
 		onLoad() {
-
+			this.getSwipers()
 		},
 		methods: {
-
+			//获取轮播图数据
+			async getSwipers() {
+				// uni.request({
+				// 	url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+				// 	success: (res) => {
+				// 		if (res.data.meta.status === 200) {
+				// 			this.swiperArr = res.data.message
+				// 		} else {
+				// 			return uni.showToast({
+				// 				title: '获取数据失败'
+				// 			})
+				// 		}
+				// 	}
+				// })
+				const res = await this.$myRequest({
+					url: '/api/public/v1/home/swiperdata'
+				})
+				this.swiperArr = res.data.message
+				console.log(res)
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.home {
+		swiper {
+			height: 380rpx;
+			width: 750rpx;
 
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.nav {
+			display: flex;
+				justify-content: space-around;
+
+			.nav-item {
+				text-align: center;
+				
+
+				view {
+					width: 120rpx;
+					height: 120rpx;
+					background-color: #b50e03;
+					border-radius: 60rpx;
+					line-height: 120rpx;
+					font-size: 60rpx;
+					color: #FFF;
+					margin: 20rpx 0;
+				}
+
+				text {
+					font-size: 30rpx;
+				}
+			}
+		}
+	}
 </style>
